9c858c0e88ad7d5628e4095b7ec310df724254bc
dpdltm2944 <102200639+dpdltm2944@users.noreply.github.com>
2022-11-04 16:38:31 +0900

    main.py

--- main.py
+++ main.py
@@ -37,6 +37,20 @@ def invalid(choice):
     logger.error(f'error: invalid input : {choice}') 
     print("잘못된 입력입니다.")
     return 0
+def exit():
+    while True:
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation.lower() == "no":
+            return False
+        elif next_calculation.lower() == "yes":
+            return True
+        else:
+            logger.error(f'error: invalid input : {next_calculation}')
+            print("say yes or no")
+            continue
+def showlog():
+    with open('logfile.log', 'r') as f:
+        print(f.read())
 
 print("Calculator started.")
 
@@ -46,6 +60,7 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide") 
 print("5.log")
+print("6.exit")
 
 while True:
     # take input from the user
@@ -76,20 +91,18 @@ while True:
             num2 = float(input("Enter second number: "))
             print(num1, "/", num2, "=", divide(num1,num2))
         elif choice =='5':
-            test = open('logfile.log','rt')
-            fileLines = test.readlines()
-            for line in fileLines:
-                print(line)
-
-            test.close()
+           showlog()
+        elif choice =='6':
+            break
            
             
 
         # check if user wants another calculation
         # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
+        if exit()==False:
             break
+       
+
 
     else:
         invalid(choice)
